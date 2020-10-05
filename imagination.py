7569e5b8fb7ad1ab50cf9642a2d1d084c60e0965
Rakesh-BCI <69778911+rakeshbci@users.noreply.github.com>
2020-10-05 21:45:50 +0530

    Update imagination.py

--- imagination.py
+++ imagination.py
@@ -1,12 +1,16 @@
 from osc4py3.as_eventloop import *
 from osc4py3 import oscmethod as osm
 import time
+
 osc_startup()
+
 def prediction(left, right):
-print(“Left prediction : “,round(left,2),”Right prediction : “,round(right,2))
+  print(“Left prediction : “,round(left,2),”Right prediction : “,round(right,2))
+ 
 osc_udp_server(“127.0.0.1”, 9002, “neuropype”)
 osc_method(“/neuropype”, prediction)
 finished = False
+
 while not finished:
-osc_process()
-osc_terminate()
+  osc_process()
+  osc_terminate()
