836db3cfae6ccc35a18aaf2e23ae0a5b3f6198c0
Rakesh-BCI <69778911+rakeshbci@users.noreply.github.com>
2020-10-09 16:25:48 +0530

    Update neuropype_recieve.py

--- neuropype_recieve.py
+++ neuropype_recieve.py
@@ -2,18 +2,10 @@ from oscpy.server import OSCThreadServer
 from time import sleep
 from oscpy.client import OSCClient
 
-accx = 0
-accy = 0
-accz = 0
-
-
 osc = OSCThreadServer()
 sock = osc.listen(address='127.0.0.1', port=9002, default=True)
 @osc.address(b'/neuropype')
 def callback(left, right):
   print("Left prediction : ",round(left,2),"Right prediction : ",round(right,2))
-
-
-
 sleep(100)
 
